093a9d8533831243e08d158f68709e64b4c5d0ee
anirudtate <anirud.programming@gmail.com>
2023-04-19 19:53:37 +0530

    back to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
-import netlify from "@astrojs/netlify/functions";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind(), mdx()],
-  adapter: netlify()
+  adapter: vercel()
 });
