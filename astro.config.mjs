d003592a4862ff54db3273dda154298594520a68
anirudtate <anirud.programming@gmail.com>
2023-04-19 19:42:03 +0530

    tring netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/edge";
 import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind(), mdx()],
-  adapter: vercel()
+  adapter: netlify()
 });
