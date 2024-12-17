13dbccf5f20f37d73eed41fc198beacb78b5f2a3
anirudtate <92687984+anirudtate@users.noreply.github.com>
2024-12-17 11:35:47 +0530

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Ref, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { motion } from "motion/react";
 
 const navElements = [
